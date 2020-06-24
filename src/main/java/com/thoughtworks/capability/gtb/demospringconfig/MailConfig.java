7d8768094a3ea22253d36e8f189b1dd2064e165a
teng.li2 <teng.li2@thoughworks.com>
2020-06-24 10:56:03 +0800

    准备演示

--- src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
@@ -2,6 +2,9 @@ package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.List;
+import java.util.Map;
+
 @ConfigurationProperties("mail")
 public class MailConfig {
 
@@ -39,6 +42,12 @@ public class MailConfig {
                 "hostname='" + hostname + '\'' +
                 ", port=" + port +
                 ", from='" + from + '\'' +
-                '}';
+                ", defaultRecipients[0]='" + defaultRecipients.get(0)+'\'' +
+                ", defaultRecipients[1]='" + defaultRecipients.get(1)+'\'' +
+                ", additionalHeaders.redelivery="+ additionalHeaders.get("redelivery")+
+                ", additionalHeaders.secure="+ additionalHeaders.get("secure")+"\n" +
+                ", credentials.username='"+credentials.getUsername()+'\''+
+                ", credentials.password='"+credentials.getPassword()+'\''+
+                ", credentials.authMethod='"+credentials.getAuthMethod()+"\'}";
     }
 }
